52f8b88ea1547fd4611e093294a75d41d6ca8c0d
yango3 <o3.yang@hotmail.com>
2018-06-15 21:33:51 +0800

    normal update

--- src/main.js
+++ src/main.js
@@ -1,2 +1,40 @@
-console.log('hi ts');
-console.log('hello');
+var x;
+x = "First Name";
+console.log(x);
+console.log(typeof x);
+var strArr;
+strArr = ['a', 'b', 'c', 'd', 'e'];
+console.log(strArr.join('-'));
+var funcMy;
+funcMy = (arg) => {
+    console.log(arg);
+    return 'This is My Func';
+};
+funcMy('A fish');
+class Character {
+    constructor(firstName, secondName) {
+        this.fullName = firstName + ' ' + secondName;
+    }
+    greet(name) {
+        if (name) {
+            return 'Hi,' + name + '. I am ' + this.fullName;
+        }
+        else {
+            return 'I am ' + this.fullName;
+        }
+    }
+}
+var charac = new Character('John', 'Smith');
+console.log(charac.greet('Joe'));
+class Logger {
+    log(arg) {
+        console.log(arg);
+    }
+}
+var custLogger = new Logger();
+custLogger.log('Hi logger');
+var userIf = {
+    name: 'normal user',
+    password: '234567'
+};
+console.log(userIf);
--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,65 @@
-console.log('hi ts');
 
-console.log('hello');
+
+var x: string | string[];
+
+x = "First Name";
+
+console.log(x);
+console.log(typeof x);
+
+type CUST_ARRAY = string[];
+var strArr: CUST_ARRAY;
+strArr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(strArr.join('-'));
+
+var funcMy: (arg: string) => string;
+funcMy = (arg: string): string => {
+    console.log(arg);
+    return 'This is My Func';
+
+};
+funcMy('A fish');
+
+class Character {
+    fullName: string;
+    constructor(firstName: string, secondName: string) {
+        this.fullName = firstName + ' ' + secondName;
+    }
+    greet(name?: string): string {
+        if (name) {
+            return 'Hi,' + name + '. I am ' + this.fullName;
+        } else {
+            return 'I am ' + this.fullName;
+        }
+    }
+}
+
+var charac = new Character('John', 'Smith');
+console.log(charac.greet('Joe'));
+
+interface LoggerInterface {
+    log(arg: any): void;
+}
+
+class Logger implements LoggerInterface {
+    log(arg: any) {
+        console.log(arg);
+    }
+}
+
+var custLogger: Logger = new Logger();
+custLogger.log('Hi logger');
+
+interface UserInterface {
+    name: string;
+    password: string;
+}
+
+var userIf: UserInterface = {
+    name: 'normal user',
+    password: '234567'
+};
+console.log(userIf);
+
+
