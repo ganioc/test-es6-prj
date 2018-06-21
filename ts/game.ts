45647a05697950158de955415049d7dd7d371b79
spike yang <ganioc.yang@gmail.com>
2018-06-21 22:38:47 +0800

    add html canvas

---
+++ ts/game.ts
@@ -0,0 +1,18 @@
+
+console.log('start');
+
+const CANVAS_ID = "mycanvas"
+const WIDTH = document.getElementById(CANVAS_ID).getAttribute('width');
+const HEIGHT = document.getElementById(CANVAS_ID).getAttribute('height')
+
+console.log('WIDTH:', WIDTH);
+console.log('HEIGHT:', HEIGHT);
+
+window.requestAnimationFrame(function (callback) {
+
+});
+
+function f() {
+  return 0;
+}
+f(); 
